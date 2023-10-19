931094ec450cf5a8cc913ca9a770954e47231dbe
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-10-18 22:26:19 -0300

    Finish implementation

--- vptree/__init__.py
+++ vptree/__init__.py
@@ -1,5 +1,15 @@
+"""
+py-vptree
+
+Authors: Nyeki <hello@nyeki.dev>
+License: MIT
+
+Copyright (c) 2022 Nyeki
+"""
+
 import sys
-import heapq
+import typing
+import secrets
 import statistics
 
 
@@ -24,9 +34,7 @@ class Node(object):
     inside: "Node" = None
     outside: "Node" = None
 
-    def __init__(
-        self, point: object, dist_fn: callable, subitems: list
-    ) -> None:
+    def __init__(self, point: object, dist_fn: callable, subitems: list) -> None:
         self.point = point
 
         if len(subitems) == 0:
@@ -40,10 +48,28 @@ class Node(object):
         outside = [d[1] for d in distances if d[0] > self.threshold]
 
         if len(inside):
-            self.inside = Node(inside.pop(0), dist_fn, inside)
+            inside_choice = secrets.choice(inside)
+            inside.remove(inside_choice)
+            self.inside = Node(inside_choice, dist_fn, inside)
 
         if len(outside):
-            self.outside = Node(outside.pop(0), dist_fn, outside)
+            outside_choice = secrets.choice(outside)
+            outside.remove(outside_choice)
+            self.outside = Node(outside_choice, dist_fn, outside)
+        
+    def insert(self, point: object, dist_fn: callable) -> None:
+        distance = dist_fn(self.point, point)
+
+        if distance < self.threshold:
+            if self.inside is None:
+                self.inside = Node(point, dist_fn, [])
+            else:
+                self.inside.insert(point, dist_fn)
+        else:
+            if self.outside is None:
+                self.outside = Node(point, dist_fn, [])
+            else:
+                self.outside.insert(point, dist_fn)
 
     def __len__(self) -> int:
         return (len(self.inside) + 1 if self.inside is not None else 0) + (
@@ -61,7 +87,7 @@ class VPTree(object):
         dist_fn (callable): The distance function to use. It takes two arguments and returns an integer.
     """
 
-    vantage_point: Node | None = None
+    vantage_point: typing.Union[Node, None] = None
 
     def __init__(self, initial: list, dist_fn: callable = hamming) -> None:
         self.dist_fn = dist_fn
@@ -69,7 +95,8 @@ class VPTree(object):
         if len(initial) == 0:
             return
 
-        vp_choice = initial.pop(0)
+        vp_choice = secrets.choice(initial)
+        initial.remove(vp_choice)
 
         distances = [self.dist_fn(vp_choice, d) for d in initial]
         self.threshold = statistics.median(distances)
@@ -80,78 +107,132 @@ class VPTree(object):
         sys.setrecursionlimit(len(initial))
 
         self.vantage_point = Node(vp_choice, dist_fn, initial)
-        
+
         # Restore the system's default recursion limit.
         sys.setrecursionlimit(recursion_limit)
 
-    def search(self, query: object, limit: int = 10) -> list:
-        """Returns the `limit` nearest points to `query`.
+    def search(
+        self, query: object, k: typing.Union[int, float]
+    ) -> typing.List[typing.Tuple[object, typing.Union[int, float]]]:
+        """Returns the k nearest neighbors to `query`.
 
         Args:
-            query (object): The query point.
-            limit (int, optional): The number of nearest points. Defaults to 10.
+            query (_type_): The query point.
+            k (int): The number of neighbors to return.
 
         Returns:
-            list: The `limit` nearest points to `query`.
+            list[tuple[object, int | float]]: A list of the k nearest neighbors to `query`. Each neighbor is a tuple of `(point, distance)`
         """
 
-        if not self.vantage_point:
-            return []
+        tau = float("inf")
+        to_search = [self.vantage_point]
 
-        nearest_neighbors = []
+        results = []
 
-        def node_search(node):
-            if node is None:
-                return
+        while to_search:
+            current_node = to_search.pop(0)
+            dist = self.dist_fn(query, current_node.point)
 
-            distance = self.dist_fn(query, node.point)
-            
-            if len(nearest_neighbors) < limit:
-                heapq.heappush(nearest_neighbors, (distance, node.point))
-            elif distance < nearest_neighbors[0][0]:
-                heapq.heappop(nearest_neighbors)
-                heapq.heappush(nearest_neighbors, (distance, node.point))
-            
-            if distance <= node.threshold:
-                node_search(node.inside)
-            
-            if distance < node.threshold or len(nearest_neighbors) < limit:
-                node_search(node.outside)
+            if dist < tau:
+                results.append((current_node.point, dist))
 
-        node_search(self.vantage_point)
+                if len(results) > k:
+                    results.sort(key=lambda node: node[1])
+                    results.pop()
+                    tau = self.dist_fn(query, results[-1][0])
 
-        return nearest_neighbors
+            if dist < current_node.threshold + tau:
+                if current_node.outside is not None:
+                    to_search.append(current_node.outside)
 
-    def radius(self, query: object, radius: int) -> list:
-        """Returns all points within `radius` of `query`.
+            if dist >= current_node.threshold - tau:
+                if current_node.inside is not None:
+                    to_search.append(current_node.inside)
+
+        return results
+
+    def radius(
+        self, point: object, radius: typing.Union[int, float]
+    ) -> typing.List[typing.Tuple[object, typing.Union[int, float]]]:
+        """Returns all points within `radius` of `point`.
 
         Args:
-            query (object): The query point.
+            point (object): The point point.
             radius (int): The maximum distance.
 
         Returns:
-            list: All points within `radius` of `query`.
+            list[tuple[object, int | float]]: All points within `radius` of `point`.
+        """
+
+        def search(node, point, tau, results):
+            if node is None:
+                return
+
+            distance = self.dist_fn(point, node.point)
+
+            if distance < tau:
+                results.append((node, distance))
+
+            if distance < node.threshold + tau:
+                search(node.inside, point, tau, results)
+
+            if distance >= node.threshold - tau:
+                search(node.outside, point, tau, results)
+
+        tau = radius
+        results = []
+        search(self.vantage_point, point, tau, results)
+        return results
+
+    def insert(self, point: object) -> None:
+        """Insert a point into the tree.
+
+        Args:
+            point (object): The point to insert.
         """
 
         if self.vantage_point is None:
-            return list()
+            self.vantage_point = Node(point, self.dist_fn, [])
+            return
 
-        def node_search(node):
-            points = []
+        self.vantage_point.insert(point, self.dist_fn)
 
-            d = self.dist_fn(query, node.point)
-            if d <= radius:
-                points.append((d, node.point))
+    def remove(self, point_to_remove: object) -> None:
+        """Remove a point from the tree.
 
-            if node.inside and d - radius <= node.threshold:
-                points += node_search(node.inside)
+        Args:
+            point_to_remove (object): The point to remove.
+        """
 
-            if node.outside and d + radius >= node.threshold:
-                points += node_search(node.outside)
+        # Helper function to remove a point from a node and restructure the tree
+        def remove_from_node(node, point_to_remove):
+            if node is None:
+                return None, False
 
-            return points
+            if node.point == point_to_remove:
+                if node.inside:
+                    replacement_point, _ = node.inside.get_nearest_neighbor(node.point)
+                    node.point = replacement_point
+                    node.inside.remove_point(point_to_remove)
+                else:
+                    return None, True
+            elif point_to_remove <= node.threshold:
+                node.inside, _ = remove_from_node(node.inside, point_to_remove)
+            else:
+                node.outside, _ = remove_from_node(node.outside, point_to_remove)
+            
+            if node.inside and node.outside:
+                node.threshold = max(node.inside.threshold, node.outside.threshold)
+            elif node.inside:
+                node.threshold = node.inside.threshold
+            elif node.outside:
+                node.threshold = node.outside.threshold
+            else:
+                node.threshold = 0
 
-        return node_search(self.vantage_point)
+            return node, False
+
+        self.vantage_point, _ = remove_from_node(self.vantage_point, point_to_remove)
 
     def __len__(self) -> int:
         return len(self.vantage_point) + 1 if self.vantage_point is not None else 0
